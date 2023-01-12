34caff6c1b263ac001dfecb544b92bf81bca11c4
ktgoldstein <60076196+ktgoldstein@users.noreply.github.com>
2023-01-12 17:39:46 -0500

    Update Graph.tsx and DataManipulator.ts to reflect implementation of new requirements

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,34 @@
 import { ServerRespond } from './DataStreamer';
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
+  price_abc: number, //for calculating ratio - graph won't show these
+  price_def: number, //for calculating ratio - graph won't show these
+  ratio: number,
   timestamp: Date,
+  upper_bound: number,
+  lower_bound: number,
+  trigger_alert: number | undefined,
 }
 
 
 export class DataManipulator {
-  static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+  static generateRow(serverRespond: ServerRespond[]): Row {
+    const priceABC = (serverRespond[0].top_ask.price + serverRespond[0].top_bid.price) / 2;
+    const priceDEF = (serverRespond[1].top_ask.price + serverRespond[1].top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const upperBound = 1 + 0.05;
+    const lowerBound = 1 - 0.05;
+
+    return {
+      price_abc: priceABC, 
+      price_def: priceDEF, 
+      ratio,
+      //if timestamp of ABC is later than DEF, return ABC timestamp, else return DEF timestamp
+      timestamp: (serverRespond[0].timestamp > serverRespond[1].timestamp) ? serverRespond[0].timestamp : serverRespond[1].timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      //return ratio or undefined if ratio is within defined bounds
+      trigger_alert: (ratio > upperBound || ratio < lowerBound ) ? ratio : undefined,
+    };
   }
 }
